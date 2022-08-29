8851d377a4dc7ab5edad4e69b9f0c9870bc54fd6
Hedgebzh <axel.gore35@gmail.com>
2022-08-29 14:45:32 +0200

    fix indents

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,7 +17,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if(tree)
+	if (tree)
 	{
 		if (binary_tree_is_leaf(tree))
 		return ((size_t)0);
