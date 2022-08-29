a7fbc3b35d22464e617df897997c688abab3b517
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-08-29 13:50:39 +0200

    adding count nodes with child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -38,5 +38,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	return (binary_tree_size - binary_tree_leaves);
+	return (binary_tree_size(tree) - binary_tree_leaves(tree));
 }
