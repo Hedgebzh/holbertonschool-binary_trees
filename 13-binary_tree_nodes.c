4a51d151576c8fef439dfeff0ada45692ce99053
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-08-29 13:49:50 +0200

    adding count nodes with child

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,42 @@
+
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * binary_tree_leaves - counts all the leaves of a tree
+ *
+ * @tree: root of tree to count
+ * Return: result
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
+/**
+ * binary_tree_size - counts amount of nodes in a tree
+ *
+ * @tree: root of tree to count
+ * Return: the result
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * binary_tree_nodes - counts nodes that have at least 1 child
+ *
+ * @tree: root of tree too count
+ * Return: result
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	return (binary_tree_size - binary_tree_leaves);
+}
