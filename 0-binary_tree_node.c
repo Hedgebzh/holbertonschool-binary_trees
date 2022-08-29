66d0912bf0ea3e338042507f3e3974c161d271fc
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-08-29 09:48:41 +0200

    adding test files

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - créates a new node fon binary tree
+ *
+ * @parent: parent of the new node
+ * @value: value in new node
+ * Return: the node, use: parent->right(or left) = binary_tree_node(p,v);
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+}
