82ab5762050541813ef5b99d4aaa6f7867a8e9f0
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-08-31 10:06:06 +0200

    addinf level order executtion

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,53 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * binary_tree_size - counts amount of nodes in a tree
+ *
+ * @tree: root of tree to count
+ * Return: the result
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+
+/**
+ * binary_tree_levelorder - goes through a tree using levelorder traversal
+ *
+ * @tree: pointer to the root of the tree to raverse
+ * @func: fonction to use on each node
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t parent = 0, child = 1, i;
+	size_t tree_size = binary_tree_size(tree);
+	const binary_tree_t *node_array[tree_size];
+
+	/*fill array with pointer to the nodes in corect order*/
+	if (tree && func)
+	{
+		node_array[0] = tree;
+		while (child < tree_size)
+		{
+			if (node_array[parent]->left)
+			{
+				node_array[child] = node_array[parent]->left;
+				child++;
+			}
+			if (node_array[parent]->right)
+			{
+				node_array[child] = node_array[parent]->right;
+				child++;
+			}
+			parent++;
+		}
+	}
+	/*execute every node*/
+	for (i = 0; i < tree_size; i++)
+		func(node_array[i]->n);
+}
