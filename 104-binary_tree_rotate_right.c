17f0f9f01892389defcd64d6f747811f168bf2ce
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-08-31 10:54:35 +0200

    adding rotate right

---
+++ 104-binary_tree_rotate_right.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * binary_tree_rotate_right - rotate right
+ *
+ * @tree: node to rotate
+ * Return: new node
+ */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+{
+	binary_tree_t *tmp = NULL;
+
+	if (tree && tree->left)
+	{
+		tmp = tree->left;
+		tree->left = tmp->right;
+		tree->parent = tmp;
+		tmp->right = tree;
+		tmp->parent = NULL;
+	}
+	return (tmp);
+}
