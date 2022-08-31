4274910172f8062a6b347f5d072529240c5a025b
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2022-08-31 14:11:16 +0200

    adding b search

---
+++ 113-bst_search.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+/**
+ * bst_search - search for node in tree
+ *
+ * @tree: tree to search in
+ * @value: value to find
+ * Return: pointer to node if found NULL othertwise
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (!tree)
+		return (NULL);
+	if (value > tree->n)
+		return (bst_search(tree->right, value));
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	return (tree);
+}
